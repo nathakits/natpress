2984b55b2b77dd47508e53422509daffad48c7a8
nathakits <nathakit.tan@gmail.com>
2019-01-17 15:00:09 +0700

    adding files from vuepress default theme

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'Pupil Labs',
+  description: ' ',
+  themeConfig: {
+    nav: [
+      { text: 'Products', link: '/products' },
+      { text: 'Cloud', link: '/cloud' },
+      { text: 'Learn', link: '/learn/' },
+      { text: 'News', link: '/news/' },
+      { text: 'Careers', link: '/careers/' },
+    ],
+    external: [
+      { text: 'Sign in', link: 'https://cloud.pupil-labs.com/sign-in'},
+      { text: 'Sign up', link: 'https://cloud.pupil-labs.com/sign-up', button: 'fill' },
+    ]
+  }
+};
